3aac4b072090516e22e63e117e4abb87a1d3d0dd
Jean Carlo Nascimento <jnascimento@gmail.com>
2016-10-30 23:21:47 -0200

    Update organism.js

--- api/modules/users/organism.js
+++ api/modules/users/organism.js
@@ -1,16 +1,8 @@
-// 'use strict';
-//
-// const mongoose  = require('mongoose');
-// const Molecule = require('./molecule');
-// const OrganismName = 'Users';
-//
-// module.exports = mongoose.model(OrganismName, Molecule);
-'use strict';
-
 const DNA = {
-	name: 'Users'
-  , organelles: ['findOneLogin']
-};
+	name: 'User', 
+	organelles: ['findOneLogin']
+}
 
-const Cell = require('./../../_factories/organism')(DNA);
-module.exports = Cell;
+const Cell = require('./../../_factories/organism')(DNA)
+module.exports = Cell
+	
